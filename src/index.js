f1d4810eff2b40a048395255559543e533f3a736
sneha suman <snehasumantezu@gmail.com>
2020-11-23 14:48:23 +0530

    changess

--- src/index.js
+++ src/index.js
@@ -12,110 +12,118 @@ const ofl = 1000000;
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
+    // your code goes here
+app.get("/", (req, res) => {
+    res.send("Hello world!");
 })
 app.post("/add", (req, res) => {
-    const num1 =Number( req.body.num1);
-    const num2 = Number(req.body.num2);
-    let notANo = false;
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    let notNumber = false;
     if (isNaN(num1) || isNaN(num2)) {
-        notANo = true;
+        notNumber = true;
     }
     let result = null;
-    if (!notANo) {
+    if (!notNumber) {
         result = num1 + num2;
     }
     let message = "the sum of given two numbers";
     let status = "success";
-    if (notANo) {
+    if (notNumber) {
         message = "Invalid data types";
         status = "error";
     }
     if (num1 < ufl || num2 < ufl || result < ufl) {
         message = "Underflow";
         status = "error";
-        notANo = true;
+        notNumber = true;
     }
     if (num1 > ofl || num2 > ofl || result > ofl) {
         message = "Overflow";
         status = "error";
-        notANo = true;
+        notNumber = true;
     }
     res.json({ status: status, message: message, sum: result });
 
 })
 
-app.post("/sub",(req,res)=>{
-    const num1 =Number( req.body.num1);
-    const num2 = Number(req.body.num2);
-    let notANo=false;
-    if(isNaN(num1) || isNaN(num2)){
-        notANo=true;
+
+app.post("/sub", (req, res) => {
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    let notNumber = false;
+    if (isNaN(num1) || isNaN(num2)) {
+        notNumber = true;
     }
-    let result=null;
-    if(!notANo){
-        result=num1-num2;
+    let result = null;
+    if (!notNumber) {
+        result = num1 - num2;
     }
-    let message="the difference of given two numbers";
-    let status="success";
-    if(notANo){
-        message="Invalid data types";
-        status="error";
+    let message = "the difference of given two numbers";
+    let status = "success";
+    if (notNumber) {
+        message = "Invalid data types";
+        status = "error";
     }
-    if(num1 <ufl || num2 <ufl ||result<ufl){
-        message="Underflow";
-        status="error";
-        notANo=true;
+    if (num1 < ufl || num2 < ufl || result < ufl) {
+        message = "Underflow";
+        status = "error";
     }
-    if(num1 >ofl || num2 >ofl ||result>ofl){
-        message="Overflow";
-        status="error";
-        notANo=true;
+    if (num1 > ofl || num2 > ofl || result > ofl) {
+        message = "Overflow";
+        status = "error";
     }
-    res.json({status:status,message:message,sum:result}); 
+    res.json({ status: status, message: message, difference: result })
+
 })
 
-app.post("/multiply",(req,res)=>{
-    const num1 =Number( req.body.num1);
-    const num2 = Number(req.body.num2);
-    let notANo=false;
-    if(isNaN(num1) || isNaN(num2)){
-        notANo=true;
+
+app.post("/multiply", (req, res) => {
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    let notNumber = false;
+    if (isNaN(num1) || isNaN(num2)) {
+        notNumber = true;
     }
-    let result=null;
-    if(!notANo){
-        result=num1*num2;
+    let result = null;
+    if (!notNumber) {
+        result = num1 * num2;
     }
-    let message="The product of given numbers";
-    let status="success";
-    if(notANo){
-        message="Invalid data types";
-        status="error";
+    let message = "The product of given numbers";
+    let status = "success";
+    if (notNumber) {
+        message = "Invalid data types";
+        status = "error";
     }
-    if(num1 <ufl || num2 <ufl ||result<ufl){
-        message="Underflow";
-        status="error";
-        notANo=true;
+    if (num1 < ufl || num2 < ufl || result < ufl) {
+        message = "Underflow";
+        status = "error";
+
     }
-    if(num1 >ofl || num2 >ofl ||result>ofl){
-        message="Overflow";
-        status="error";
-        notANo=true;
+    if (num1 > ofl || num2 > ofl || result > ofl) {
+        message = "Overflow";
+        status = "error";
     }
-    res.json({status:status,message:message,sum:result}); 
+    res.json({ status: status, message: message, result: result });
+
 })
 
-app.post("/divide",(req,res)=>{
-    const num1 =Number( req.body.num1);
-    const num2 = Number(req.body.num2);
-    let notANo=false;
-    if(isNaN(num1) || isNaN(num2)){
-        notANo=true;
+
+app.post("/divide", (req, res) => {
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    let notNumber = false;
+    if (isNaN(num1) || isNaN(num2)) {
+        notNumber = true;
     }
-    let result=null;
-    if(!notANo){
+    let result = null;
+    let message = "The division of given numbers";
+    let status = "success";
+    if (notNumber) {
+        message = "Invalid data types";
+        status = "error";
+    }
+    if (!notNumber) {
         if (num2 === 0) {
             message = "Cannot divide by zero";
             status = "error";
@@ -124,25 +132,19 @@ app.post("/divide",(req,res)=>{
             result = num1 / num2;
         }
     }
-    let message="The division of given numbers";
-    let status="success";
-    if(notANo){
-        message="Invalid data types";
-        status="error";
+    if (num1 < ufl || num2 < ufl || result < ufl) {
+        message = "Underflow";
+        status = "error";
+        notNumber = true;
     }
-    if(num1 <ufl || num2 <ufl ||result<ufl){
-        message="Underflow";
-        status="error";
-        notANo=true;
+    if (num1 > ofl || num2 > ofl || result > ofl) {
+        message = "Overflow";
+        status = "error";
+        notNumber = true;
     }
-    if(num1 >ofl || num2 >ofl ||result>ofl){
-        message="Overflow";
-        status="error";
-        notANo=true;
-    }
-    res.json({status:status,message:message,sum:result}); 
-})
+    res.json({ status: status, message: message, result: result });
 
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
